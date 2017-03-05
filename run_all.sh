4e7ba178dc19cff87ceb2e51e73bbc8c382c06dc
Jarred de Beer <jarr2000@gmail.com>
2017-03-05 22:53:29 +0200

    06 - Use Fast Math

--- run_all.sh
+++ run_all.sh
@@ -16,7 +16,9 @@ run_simulations() {
 
 run_simulations SERIAL timing/singlethreaded        ./bin/serial.adjoint           assets.rainbow.in
 run_simulations OMP    timing/multithreaded.omp     ./bin/omp.adjoint              assets.rainbow.in
-run_simulations CUDA01 timing/multithreaded.cuda.01 ./bin/cuda.01.adjoint          assets.rainbow.in
-run_simulations CUDA02 timing/multithreaded.cuda.02 ./bin/cuda.02.adjoint          assets.rainbow.in
-run_simulations CUDA03 timing/multithreaded.cuda.03 ./bin/cuda.03.floats           assets.rainbow.in
-run_simulations CUDA04 timing/multithreaded.cuda.04 ./bin/cuda.03.float.constants  assets.rainbow.in
+run_simulations CUDA01 timing/multithreaded.01.cuda ./bin/cuda.01.adjoint          assets.rainbow.in
+run_simulations CUDA02 timing/multithreaded.02.cuda ./bin/cuda.02.adjoint          assets.rainbow.in
+run_simulations CUDA03 timing/multithreaded.03.cuda ./bin/cuda.03.floats           assets.rainbow.in
+run_simulations CUDA04 timing/multithreaded.04.cuda ./bin/cuda.04.float.constants  assets.rainbow.in
+run_simulations CUDA05 timing/multithreaded.05.cuda ./bin/cuda.05.shared.memory    assets.rainbow.in
+run_simulations CUDA06 timing/multithreaded.06.cuda ./bin/cuda.06.fast.math        assets.rainbow.in
