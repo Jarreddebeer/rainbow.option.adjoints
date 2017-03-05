abc7dcaa50e22490803905d026ca6257f9cc4613
Jarred de Beer <jarr2000@gmail.com>
2017-03-05 20:29:22 +0200

    01 - Philox random number generator

--- run_all.sh
+++ run_all.sh
@@ -14,5 +14,7 @@ run_simulations() {
     done
 }
 
-run_simulations SERIAL timing/singlethreaded       ./bin/serial.adjoint assets.rainbow.in
-run_simulations OMP    timing/multithreaded.omp    ./bin/omp.adjoint    assets.rainbow.in
+run_simulations SERIAL timing/singlethreaded        ./bin/serial.adjoint  assets.rainbow.in
+run_simulations OMP    timing/multithreaded.omp     ./bin/omp.adjoint     assets.rainbow.in
+run_simulations CUDA01 timing/multithreaded.cuda.01 ./bin/cuda.01.adjoint assets.rainbow.in
+run_simulations CUDA02 timing/multithreaded.cuda.02 ./bin/cuda.02.adjoint assets.rainbow.in
